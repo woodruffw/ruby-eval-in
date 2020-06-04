1a0d9686804b932695b069dc9072158f0bfebb03
William Woodruff <william@yossarian.net>
2020-06-03 22:12:41 -0400

    eval-in: 0.0.8

--- lib/eval-in.rb
+++ lib/eval-in.rb
@@ -8,7 +8,7 @@ require_relative "eval-in/exceptions"
 # @since 0.0.1
 module EvalIn
   # The library's current version.
-  VERSION = "0.0.7"
+  VERSION = "0.0.8"
 
   # Evaluate some code in a given language.
   # @example
